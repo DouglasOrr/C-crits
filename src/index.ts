d809400d7ba265958ba99d866c573be1a065dc00
Douglas Orr <douglas.orr99@gmail.com>
2024-11-04 02:05:05 +0000

    Create space for a code editor

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,10 @@ const S = {
 };
 
 window.onload = () => {
+  const container = document.getElementById("col-sim")!;
   const scene = new THREE.Scene();
   scene.background = new THREE.Color(0x000000);
-
-  const aspect = window.innerWidth / window.innerHeight;
+  const aspect = container.offsetWidth / container.offsetHeight;
   const camera = new THREE.OrthographicCamera(
     -S.hwidth,
     S.hwidth,
@@ -16,23 +16,20 @@ window.onload = () => {
     -S.hwidth / aspect
   );
   camera.position.z = 10;
-
   const renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
+  renderer.setSize(container.offsetWidth, container.offsetHeight);
+  container.appendChild(renderer.domElement);
   window.addEventListener("resize", () => {
-    const aspect = window.innerWidth / window.innerHeight;
+    const aspect = container.offsetWidth / container.offsetHeight;
     camera.top = S.hwidth / aspect;
     camera.bottom = -S.hwidth / aspect;
     camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(container.offsetWidth, container.offsetHeight);
   });
-
   const map = new THREE.TextureLoader().load("textures/crit.png");
   const sprite = new THREE.Sprite(new THREE.SpriteMaterial({ map: map }));
   scene.add(sprite);
   sprite.scale.set(5, 5, 1);
-
   renderer.setAnimationLoop(() => {
     sprite.position.x += 1 / 60;
     sprite.position.y += 1 / 60;
